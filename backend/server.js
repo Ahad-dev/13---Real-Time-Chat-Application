22bf3d9b8a715e5fd20a0646289c7561e4cbd0fd
Muhammad Ahad <muhammadahad211126@gmail.com>
2024-08-26 18:33:33 +0500

    add vercel.json

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'ChatyApp.vercel.app', // Work: true for all origins
     credentials: true
 }));
 app.use(express.json()); // Work: Parse JSON bodies (as sent by API clients)
